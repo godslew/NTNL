5d91f517df6e810d1949f618a4a161a89c5503e9
godslew <maousnake00@live.jp>
2015-04-17 14:12:10 +0900

    add class to models

---
+++ NTNL/Models/init/DBCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *ここにDBを初期化するものを書く
+ */
+namespace NTNL.Models.init
+{
+    class DBCreator
+    {
+    }
+}
