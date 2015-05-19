d83f4ba70d5ca2e77276f89fb67236eb80d16e8f
Kimura0726 <kimura4976@yahoo.co.jp>
2015-05-19 19:59:25 +0900

    little fix

--- NTNL/Models/DB/DAO/EntityDAO.cs
+++ NTNL/Models/DB/DAO/EntityDAO.cs
@@ -17,19 +17,21 @@ namespace NTNL.Models.DB.DAO
         {
             this.primaryColumnName = primaryColumnName;
         }
-        
-        protected SQLiteDataReader find(int id)
-        {
-            Dictionary<String,Object> where = new Dictionary<String, Object>();
-            where[this.primaryColumnName] = id;
-            return this.select(where);
-        }
+        /*       
+                protected SQLiteDataReader find(int id)
+                {
+                    Dictionary<String,Object> where = new Dictionary<String, Object>();
+                    where[this.primaryColumnName] = id;
+                    return this.select(where);
+                }
 
-        protected SQLiteDataReader findIdentity(String columnName, Object value)
-        {
-            Dictionary<String, Object> where = new Dictionary<string, object>();
-            where[columnName] = value;
-            return this.select(where);
-        }
+                protected SQLiteDataReader findIdentity(String columnName, Object value)
+                {
+                    Dictionary<String, Object> where = new Dictionary<string, object>();
+                    where[columnName] = value;
+                    return this.select(where);
+                }
+            }
+        */
     }
 }
